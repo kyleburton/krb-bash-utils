311e94564f999049d278c432e55f98025e7c6845
Kyle Burton <kyle.burton@algorithmics.com>
2010-03-13 14:34:19 -0500

    added my .gitconfig, mroot and proot shell utils for going back to the current projects root dir

--- git-prompt.bash
+++ git-prompt.bash
@@ -18,4 +18,10 @@ function parse_git_branch {
   git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e "s/* \(.*\)/[$(git_pwd_is_tracked)\1$(parse_git_dirty)]/"
 }
 
-export PS1='\u@\h \[\033[1;33m\]\w\[\033[0m\]$(parse_git_branch)$ '
+lc_hostname=$(hostname | tr A-Z a-z | cut -f1 -d.)
+export PS1='\u@$lc_hostname \[\033[1;33m\]\w\[\033[0m\]$(parse_git_branch)$ '
+
+
+# see also http://git.kernel.org/?p=git/git.git;a=blob;f=contrib/completion/git-completion.bash;h=5543dc4d14e22b065cbc41ed8af0f695c0464152;hb=HEAD
+# see also git `symblic-ref HEAD'
+
